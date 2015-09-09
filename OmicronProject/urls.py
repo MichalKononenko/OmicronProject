0735c3c96f8dd1c5b5172519294de894d64c4ac7
shahab <shahab.uweng@gmail.com>
2015-09-09 19:29:59 -0400

    fixed auth url patterns

--- OmicronProject/urls.py
+++ OmicronProject/urls.py
@@ -8,12 +8,12 @@ from authentication.views import register, login, logout
 
 router = routers.SimpleRouter()
 router.register(r'projects', ProjectsViewSet)
-router.register(r'register', register)
-router.register(r'login', login)
-router.register(r'logout', logout)
 
 urlpatterns = patterns('',
     url(r'^api/', include(router.urls)),
+    url(r'^api/register/$', register),
+    url(r'^api/login/$', login),
+    url(r'^api/logout/$', logout),
     url(r'^dashboard/', omicron_views.dashboard, name='dashboard'),
     url(r'^userdash/', omicron_views.userdash, name='userdash'),
     url(r'^admin/', include(admin.site.urls)),
