9e8e228946cd0fc8b76d54f09673ddabde863cdc
shahab <shahab.uweng@gmail.com>
2015-09-09 19:11:08 -0400

    added Django Authentication framework

--- OmicronProject/urls.py
+++ OmicronProject/urls.py
@@ -4,9 +4,13 @@ from rest_framework import routers
 
 from OmicronProject import views as omicron_views
 from projects.views import ProjectsViewSet
+from authentication.views import register, login, logout
 
 router = routers.SimpleRouter()
 router.register(r'projects', ProjectsViewSet)
+router.register(r'register', register)
+router.register(r'login', login)
+router.register(r'logout', logout)
 
 urlpatterns = patterns('',
     url(r'^api/', include(router.urls)),
