2367b24fcba842bf6d48a72588cb0f28e674cb74
Michal Kononenko <michalkononenko@gmail.com>
2015-10-23 11:38:48 -0400

    Testing with test case

--- projects/tests.py
+++ projects/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
-
 # Create your tests here.
+from django.test import TestCase
+import projects.models as models
+
+
+class TestProjects(TestCase):
+    def setUp(self):
+        self.title = 'TestProject'
+        self.supervisor = 'TestSupervisor'
+        models.Projects.objects.create(
+            title=self.title, supervisor=self.supervisor
+        )
+
+    def test_str(self):
+        project = models.Projects.objects.get(title=self.title)
+        self.assertEqual(self.title, str(project))
